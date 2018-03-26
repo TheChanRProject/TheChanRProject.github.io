2e4fb900767083343388c4caac5896076499dabc
Rishov Chatterjee <garpchoo@gmail.com>
2018-03-26 11:03:47 -0700

    adding files

---
+++ tutorial/js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('button').onclick = () => {
+  document.getElementById('display').innerHTML = parseInt(document.getElementById('num2').innerHTML) + parseInt(document.getElementById('num1').innerHTML)
+}
